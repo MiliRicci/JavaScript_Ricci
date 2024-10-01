9ce6922326661ee5e6923942283a8d6a12e3f47a
MiliRicci <miliricci2005@gmail.com>
2024-10-01 08:17:19 -0300

    Create Ejercicio2.js

---
+++ Ejercicio2.js
@@ -0,0 +1,11 @@
+//Ej 2 - JavaScript
+
+let num1 = prompt("Ingrese un número:");
+num1 = Number(num1);
+
+let num2 = prompt("Ingrese otro número:");
+num2 = Number(num2);
+
+let suma = num1 + num2;
+
+console.log("La suma entre los dos números es: " + suma);
