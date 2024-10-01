8c3e508b2956c3f999ef328ff61eadbb393c48ba
MiliRicci <miliricci2005@gmail.com>
2024-10-01 08:18:22 -0300

    Create Ejercicio3.js

---
+++ Ejercicio3.js
@@ -0,0 +1,14 @@
+// Ej3 - JavaScript
+
+let string1 = prompt("Ingresa el primer string:");
+
+let string2 = prompt("Ingresa el segundo string:");
+
+// Comparar si son iguales
+let igual = (string1 === string2);
+
+// Comparar longitud 
+let long = (string1.length === string2.length);
+
+console.log("¿Los strings son iguales?: " + igual);
+console.log("¿Los strings tienen la misma longitud?: " + long);
